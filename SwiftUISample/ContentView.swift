5af1ee28eee3d9e95c32ba30ae66eb43d0df77ae
sohee <sorrri.b@gmail.com>
2019-06-06 09:20:38 +0900

    Fixed List and Added Navigation View

--- SwiftUISample/ContentView.swift
+++ SwiftUISample/ContentView.swift
@@ -11,16 +11,17 @@ import AVFoundation
 
 struct ContentView : View {
     
-    var video: Video = videoList[0]
+    var videos = videoList
+    var currentVideo = videoList[0]
     
     var body: some View {
-        VStack {
-            PlayerView(video: video)
-                .frame(height: 300)
-            
-            List(videoList) { video in
-                Text(video.title)
+        NavigationView {
+            VStack {
+                PlayerView(video: currentVideo)
+                    .frame(height: 300)
+                VideoListView(videos: videos)
             }
+            .navigationBarTitle(Text(currentVideo.title))
         }
     }
 }
@@ -28,7 +29,21 @@ struct ContentView : View {
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView(video: videoList[0])
+        ContentView()
     }
 }
 #endif
+
+struct VideoListView : View {
+    
+    var videos: [Video]
+    var body: some View {
+        return List(videos) { video in
+            VStack {
+                VideoRow(video: video)
+                    .padding(.top, 10)
+                    .padding(.bottom, 10)
+            }
+        }
+    }
+}
